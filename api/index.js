51439e956bdecd2862dcd5be95cbf7b8d61b0285
Nico <nicoamicone1@gmail.com>
2022-08-01 21:17:08 -0300

    socketio

--- api/index.js
+++ api/index.js
@@ -4,27 +4,43 @@ import typeDefs from "./typeDefs.js";
 import resolvers from "./resolvers.js";
 import express, { json, urlencoded } from "express";
 import cors from "cors";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import { Server } from "socket.io";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: false }));
 
-app.get("/",(req,res)=>res.send("SBS Challenge API"))
+app.get("/", (req, res) => res.send("SBS Challenge API"));
 
-async function start() {
-  const server = new ApolloServer({ typeDefs, resolvers });
-  await server.start();
+const server = new ApolloServer({ typeDefs, resolvers });
+await server.start();
 
-  server.applyMiddleware({ app });
+server.applyMiddleware({ app });
 
-  app.listen(process.env.PORT, () => {
-    console.log(`Server ready`, process.env.PORT);
+let httpServer = app.listen(process.env.PORT, () => {
+  console.log(`Server running at http://localhost:${process.env.PORT}\n\n`);
+});
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.emit(
+    "reload",
+    "query { allProducts { _id, name, price, description, image_url } }"
+  );
+  socket.on("getall", () => {
+    io.emit(
+      "reload",
+      "query { allProducts { _id, name, price, description, image_url } }"
+    );
   });
-}
-start();
-
-export default app;
+});
